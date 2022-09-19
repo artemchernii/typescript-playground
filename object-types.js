b08a75a5a8e97440c1404e5e6c7aa10e9bd60d2f
Artem Chernii <104429440+artemcherniiosf@users.noreply.github.com>
2022-09-19 14:24:33 +0200

    feat: play with object types

---
+++ object-types.js
@@ -0,0 +1,8 @@
+"use strict";
+const artem = {
+    name: 'Artem',
+    age: 22,
+    prof: 'fe dev',
+    girl: true,
+};
+console.log('artem', artem);
---
+++ object-types.ts
@@ -0,0 +1,13 @@
+const artem: {
+  name: string;
+  age: number;
+  prof?: string;
+  girl: undefined | boolean;
+} = {
+  name: 'Artem',
+  age: 22,
+  prof: 'fe dev',
+  girl: true,
+};
+
+console.log('artem', artem);
