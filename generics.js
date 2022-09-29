a871c629b0284c7c248cd92c3504bb423356b48a
Artem Chernii <104429440+artemcherniiosf@users.noreply.github.com>
2022-09-29 11:57:53 +0200

    feat: working with generics and interfaces

--- generics.js
+++ generics.js
@@ -19,3 +19,28 @@ const whatIsBool = checkTheType(boolCheck);
 const whatIsNull = checkTheType(nullCheck);
 console.log('whatIsNull', whatIsNull);
 console.log('whatIsBool', whatIsBool);
+const rustamUser = { name: 'Rustam', type: 5 };
+class WorkWithCustomer {
+    type;
+    name;
+    constructor(type, name) {
+        this.type = type;
+        this.name = name;
+    }
+}
+const workWithCustomer1 = new WorkWithCustomer('new', 'Artem');
+function workWithPerson(args) {
+    if (Object.entries(args).length > 1) {
+        console.log('Object has more than one key');
+        for (const key in args) {
+            if (Object.prototype.hasOwnProperty.call(args, key)) {
+                const element = args[key];
+                console.log(`key:${key} | element: ${element}`);
+            }
+        }
+    }
+    return args.name;
+}
+const wwp = workWithPerson({ name: 'Artem' });
+const testDef = workWithPerson({ type: 'a', name: 'Ahtung' });
+console.log(testDef);
